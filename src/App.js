6ad7eac12c77f7a80cd2b155ae65373d46406b34
Joe <joebh5@gmail.com>
2017-12-18 21:18:08 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='background'>
+        <div className='shape'>
+          <p className='text'>under construction👷‍♂</p>
+        </div>
+        <div className='shape'>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
