938f6d521028566d2b67bf3fa808019ccb9968ed
Joe <joebh5@gmail.com>
2018-01-14 23:22:27 -0600

    made the personal site...personal

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import './App.css';
 class App extends Component {
   	render() {
     	return (
-      		<div className='background'>
-        		<div className='area'>
-          			<span className='text'>under construction<span role="img" aria-label="construction">👷‍</span></span>
-        		</div>
+      		<div className="background">
+        		<div className="area">
+          			<div className="white heading">Joseph B. Higgs V <span role="img" aria-label="construction">👨‍💻</span></div>
+        		    <div className="white icons">
+                    <a href="https://www.github.com/joehiggs" target="_blank">
+                        <i className="fa fa-2x fa-github" aria-hidden="true"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/higgsvio" target="_blank">
+                        <i className="fa fa-2x fa-linkedin" aria-hidden="true"></i>
+                    </a>
+                    <a href="http://www.opico.io" target="_blank">
+                        <i className="fa fa-2x fa-map-marker" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
       		</div>
     	);
   	}
